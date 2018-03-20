ee7bebb07206285da37a4002efa3470cae5c8da4
Anders Klever Kirkeby <anders@klever-kirkeby.com>
2018-03-20 19:41:15 +0100

    Add repr  to usermodel

--- ntnui/apps/data_storage/models/user.py
+++ ntnui/apps/data_storage/models/user.py
@@ -4,3 +4,6 @@ from django.db import models
 class UserModel(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def __repr__(self):
+        return self.first_name.title() + " " + self.last_name.title()
