2897ab8a8f8593c8c9bdd364509396399ccce9a0
Jan Jakub Burak <janjb@stud.ntnu.no>
2018-10-18 12:22:34 +0200

    Merge fix

--- ntnui/apps/forms/views/__init__.py
+++ ntnui/apps/forms/views/__init__.py
@@ -1,4 +1,3 @@
-from .coach_view import *
 from .form_text_saver_view import *
 from .form_text_test_view import *
 from .list_view import *
--- ntnui/apps/forms/views/instantiator_view.py
+++ ntnui/apps/forms/views/instantiator_view.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.http import HttpResponse
 from ..form_types import instantiation_forms
 from forms.models import FormTextModel
+from forms.actions import Actions
 
 class InstantiatorView(View):
 
@@ -16,6 +17,8 @@ class InstantiatorView(View):
     def post(self, request, slug):
         form = instantiation_forms[slug](request.POST)
         model_instance = form.save(commit=True)
+        actions = Actions(model_instance)
+        actions.do()
         # get formtext with highest primary key
         form_text_id = FormTextModel.objects.filter(form_slug=slug).last().id
         setattr(model_instance, 'meta_version', form_text_id)
