41e38c6613696d1e52fbca1506913d6fa7b47b2a
Jan Jakub Burak <janjb@stud.ntnu.no>
2018-10-18 11:17:31 +0200

    Change form_name in formtextmodel to form_slug

--- ntnui/apps/forms/models/form_text_model.py
+++ ntnui/apps/forms/models/form_text_model.py
@@ -3,12 +3,12 @@ from django import forms
 
 class FormTextModel(models.Model):
 
-    form_name = models.CharField(max_length=200)
+    form_slug = models.CharField(max_length=200)
     form_text_content = models.TextField()
     form_text_version_number = models.IntegerField(default=0)
 
     def get_form_name(self):
-        return self.form_name
+        return self.form_slug
 
     def get_form_(self):
         return self.form_text_content
