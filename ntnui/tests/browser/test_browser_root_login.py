c6907fe3e9e08e45f533acdea9c439784606cc48
Jonathan Siggaard Linnestad <jonathan.s.linnestad@gmail.com>
2018-10-18 11:07:27 +0200

    fix make docker_browser_test_

--- ntnui/tests/browser/test_browser_root_login.py
+++ ntnui/tests/browser/test_browser_root_login.py
@@ -5,18 +5,20 @@ from ntnui.tests.browser.lib.browser_test_case import ChromeTestCase, FirefoxTes
 def login_general(browser, server_url, assertEquals, assertTrue):
     browser.get(server_url + '/login/')
     username_input = browser.find_element_by_name('username')
-    username_input.send_keys(settings.DUMMY_USER_EMAIL)
+    username_input.send_keys("super@admin.com")
     password_input = browser.find_element_by_name('password')
-    password_input.send_keys(settings.DUMMY_USER_PASSWORD)
-    browser.find_element_by_xpath('//button[text()="Log in"]').click()
-    account_p = browser.find_element_by_xpath('//p[text()="Account"]')
-    assertEquals(account_p.text, 'Account')
+    password_input.send_keys("supersuper")
+    browser.find_element_by_xpath('//button').click()
+    browser.save_screenshot('screenie.png')
+    account_p = browser.find_element_by_xpath("//a[contains(text(),'super@admin.com')]")
+    
+    assertEquals(account_p.text.lower(), 'super@admin.com')
     assertTrue(
-        'href="/logout/">Log out</a>' in browser.page_source)
+        'href="/a/logout/"' in browser.page_source)
 
 
 class LoginChrome(ChromeTestCase):
-    fixtures = ['users.json']
+    fixtures = ['database.json']
 
     def test_login_chrome(self):
         login_general(self.chrome, self.server_url,
@@ -24,7 +26,7 @@ class LoginChrome(ChromeTestCase):
 
 
 class LoginFirefox(FirefoxTestCase):
-    fixtures = ['users.json']
+    fixtures = ['database.json']
 
     def test_login_chrome(self):
         login_general(self.firefox, self.server_url,
