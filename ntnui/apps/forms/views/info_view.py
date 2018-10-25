e2d23a7f58f70433695882eed7317931c15f5be2
andesal <andesal@stud.ntnu.no>
2018-10-25 14:03:50 +0200

    Remove unused imports, sort lists in list_view, archived_list_view, change PermissionDenied to HttpResponseDenied, change try/catch to get_object_or_404

--- ntnui/apps/forms/views/info_view.py
+++ ntnui/apps/forms/views/info_view.py
@@ -1,28 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from forms.models import FormTextModel, AbstractFormModel
-from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
-from django.http import Http404
-
+from django.http import HttpResponseForbidden
 
 class InfoView(View):
     def get(self, request, slug, id):
-        try:
-            form_instance = AbstractFormModel.objects.get(id=id)
-            form_text = FormTextModel.objects.get(id=form_instance.meta_version)
-            signatures = form_instance.form_signatures.all()
-            signers = form_instance.form_signers.all()
-            user = request.user
-            print(signatures)
-            print(signers)
-            if user in signatures or user not in signers:
-                raise PermissionDenied
-            context = {
-                'form_id': id,
-                'form_text': form_text.form_text_content,
-                'form_slug': slug
-            }
-            return render(request, 'form_info.html', context)
-        except ObjectDoesNotExist:
-            raise Http404("Could not find form record")
+        form_instance = get_object_or_404(AbstractFormModel, pk=id)
+        form_text = FormTextModel.objects.get(id=form_instance.meta_version)
+        signatures = form_instance.form_signatures.all()
+        signers = form_instance.form_signers.all()
+        user = request.user
+        print(signatures)
+        print(signers)
+        if user in signatures or user not in signers:
+            return HttpResponseForbidden()
+        context = {
+            'form_id': id,
+            'form_text': form_text.form_text_content,
+            'form_slug': slug
+        }
+        return render(request, 'form_info.html', context)
 
--- ntnui/apps/forms/views/signed_form_view.py
+++ ntnui/apps/forms/views/signed_form_view.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from ..models import AbstractFormModel, FormTextModel
 from ..utils import form_utils
-from django.core.exceptions import PermissionDenied, ObjectDoesNotExist
-from django.http import Http404
+from django.http import HttpResponseForbidden
 from ..form_types import *
 
 
 class SignedFormView(View):
 
     def get(self, request, slug, id):
-        try:
             current_user = request.user
-            record = AbstractFormModel.objects.get(id=id)
+            record = get_object_or_404(AbstractFormModel, pk=id)
             if current_user not in record.form_signers.all():
-                raise PermissionDenied
+                return HttpResponseForbidden()
 
             form_text = FormTextModel.objects.get(id=record.meta_version)
             form = signing_forms[slug](request.POST or None, instance=record)
@@ -27,5 +25,4 @@ class SignedFormView(View):
                 'is_authorized': form_utils.is_authorized(current_user),
             }
             return render(request, 'form_signed_info.html', context)
-        except ObjectDoesNotExist:
-            raise Http404("Could not find form record")
+      
