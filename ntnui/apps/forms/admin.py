1a4ab2be99059a42f0780069ad0f95857d2dfb47
andesal <andesal@stud.ntnu.no>
2018-10-30 15:01:10 +0100

    Add TeamContract form

--- ntnui/apps/forms/admin.py
+++ ntnui/apps/forms/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import AbstractFormModel
-from .models import CoachFormModel
-from .models import FormTextModel
+from .models import AbstractFormModel, CoachFormModel, FormTextModel, TeamContractFormModel
 
 admin.site.register(AbstractFormModel)
 admin.site.register(CoachFormModel)
 admin.site.register(FormTextModel)
+admin.site.register(TeamContractFormModel)
--- ntnui/apps/forms/models/__init__.py
+++ ntnui/apps/forms/models/__init__.py
@@ -1,3 +1,4 @@
 from .abstract_form_model import *
 from .form_text_model import *
 from .coach_form_model import *
+from .teamcontract_form_model import *
